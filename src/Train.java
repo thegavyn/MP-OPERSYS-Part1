709d6adf3d7800d0917c53fc83cea146897f69ca
Gavin Sanchez <mark_gavin_sanchez@dlsu.edu.ph>
2017-07-19 14:01:43 +0800

    Add files via upload

--- src/Train.java
+++ src/Train.java
@@ -5,59 +5,19 @@
 import java.util.ArrayList;
 
 public class Train extends Thread{
-    public Lock[] seats;
     public ArrayList<Passenger> passengerArrayList;
     public Station currStation;
-    public static int spawned;
-    public Rail rail;
+    public static int numberPass;
 
     public Train(int cap)
     {
-        spawned++;
-        this.start();
-        seats = new Lock[cap];
+        passengerArrayList = new ArrayList<Passenger>();
+        numberPass = cap;
     }
 
-    public void loadTrain() {
-        if (currStation.getWaiting().isEmpty()) {
-            rail = rail.nextRail;
-        } else { // kung may tambay sa station
-            int currFreeSeats = countFreeSeats(); // count free seats
-            for (int x = 0; x < currStation.getWaiting().size(); x++) {
-                currStation.getWaiting().get(x).start();//will start thread for passenger
-                if (currFreeSeats > 0) { // if train has enough free seats
-                    currStation.getWaiting().get(x).onBoard(this);         // put passenger on board
-                    passengerArrayList.add(currStation.getWaiting().get(x)); // put passenger on board
-                    currStation.getWaiting().remove(x); // remove passenger from station
-                    // lock seat (how do i do this)
-                    Lock a = new Lock();
-                    a.lock();
-                    seats[x] = a;
-
-                }
-
-            }
-        }
-    }
-
-    public void unloadTrain()
-    {
-        for (int x = 0; x < passengerArrayList.size(); x++) {
-            if (passengerArrayList.get(x).destinationStation == currStation) {
-                passengerArrayList.get(x).getOff();
-                passengerArrayList.remove(x);
-            }
-        }
-    }
 
     public int countFreeSeats() {
-        int freeSeats = 0;
-        for (int x = 0; x < seats.length; x++) {
-            if (!seats[x].lockState) { // check if which seats are not locked
-                freeSeats++;
-            }
-        }
-        return freeSeats;
+        return numberPass - passengerArrayList.size();
     }
 
 }
